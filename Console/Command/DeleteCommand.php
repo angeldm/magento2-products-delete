e54b03c35f46f6054e20b054f0ffa05262c5678b
Àngel Díaz <angeldm@gmail.com>
2019-04-10 01:50:09 +0200

    first commit

---
+++ Console/Command/DeleteCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Angeldm\ProductsDelete\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DeleteCommand extends Command
+{
+
+    const NAME_ARGUMENT = "name";
+    const NAME_OPTION = "option";
+
+    protected $_objectManager;
+    protected $_registry;
+    protected $_productCollectionFactory;
+    protected $_productRepository;
+
+    public function __construct(
+	    \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+            \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+	    \Magento\Framework\Registry $registry
+  
+    ) {
+	    $this->_registry = $registry;
+	    $this->_productRepository = $productRepository;
+	$this->_objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+	$this->_productCollectionFactory = $productCollectionFactory;
+	parent::__construct();
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $name = $input->getArgument(self::NAME_ARGUMENT);
+        $option = $input->getOption(self::NAME_OPTION);
+	$this->deleteAllProducts();
+	$this->deleteAllCategories();
+	$output->writeln("Hello " . $name);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("products:delete");
+        $this->setDescription("delete");
+        $this->setDefinition([
+            new InputArgument(self::NAME_ARGUMENT, InputArgument::OPTIONAL, "Name"),
+            new InputOption(self::NAME_OPTION, "-a", InputOption::VALUE_NONE, "Option functionality")
+        ]);
+        parent::configure();
+    }
+
+    private function deleteAllCategories() {
+    	$categoryFactory = $this->_objectManager->get('Magento\Catalog\Model\CategoryFactory');
+	$newCategory = $categoryFactory->create();
+	$collection = $newCategory->getCollection();
+	$this->_registry->register("isSecureArea", true);
+	foreach($collection as $category) {
+	        if($category->getId() > 2)
+         $category->delete();
+    	}
+    }
+
+    private function deleteAllProducts(){
+	$collection = $this->_productCollectionFactory->create();
+	$collection->addAttributeToSelect('*');
+	$collection->load();
+ 
+	$this->_registry->register("isSecureArea", true);
+	foreach ($collection as $product){
+        	 $this->_productRepository->deleteById($product->getSku());   
+	}    
+	
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Angeldm_ProductsDelete',
+    __DIR__
+);
